d9ae8bc03a68eca50c02e05888cf127254f4603c
Nick <nick98_molo@icloud.com>
2022-08-13 15:51:42 +0300

    Added allure reporter.

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -4,6 +4,7 @@ import "./commands/forWeather";
 import "./commands/forSelectIphone";
 import "./commands/forModelCar";
 import "./commands/forLoginAndRegistration";
+require("@shelex/cypress-allure-plugin");
 Cypress.on("uncaught:exception", (err, runnable) => {
     return false;
 });
